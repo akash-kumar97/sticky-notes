7fe44e8b6020759639945c46d2ab7fa04f40debb
Akash kumar <choudharyakash170@gmail.com>
2024-07-25 14:26:47 +0530

    added useRef to change the size of notes

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,16 +1,48 @@
+import { useEffect, useRef } from "react";
+import Trash from "../icons/Trash";
 
 const NoteCard = ({ note }) => {
     const position = JSON.parse(note.position);
     const colors = JSON.parse(note.colors);
     const body = JSON.parse(note.body);
 
+    const textAreaRef = useRef(null);
+
+    useEffect(() => {
+        autoGrow(textAreaRef);
+    }, [])
+
+    const autoGrow = (textAreaRef) => {
+        const { current } = textAreaRef;
+        current.style.height = "auto";//reset the height
+        current.style.height = current.scrollHeight + "px";
+    }
+
+
     return (
         <div
             className="card"
             style={{
                 backgroundColor: colors.colorBody,
+                left: `${position.x}px`,
+                top: `${position.y}px`,
             }}
-        >{body}</div>
+        >
+            <div
+                className="card-header"
+                style={{ backgroundColor: colors.colorHeader }}
+            >
+                <Trash />
+            </div>
+            <div className="card-body">
+                <textarea
+                    style={{ color: colors.colorText }}
+                    defaultValue={body}
+                    ref={textAreaRef}
+                    onInput={() => autoGrow(textAreaRef)}
+                ></textarea>
+            </div>
+        </div >
     )
 }
 
