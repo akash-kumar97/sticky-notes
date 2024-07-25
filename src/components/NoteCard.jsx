9c1555eb9edb44918c440aa097e030f372bbd3bb
Akash kumar <choudharyakash170@gmail.com>
2024-07-25 14:37:38 +0530

    added mouseUp and mouseDown functionality, to move notes from one place to another

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,12 +1,48 @@
-import { useEffect, useRef } from "react";
+/* eslint-disable react/prop-types */
+import { useEffect, useRef, useState } from "react";
 import Trash from "../icons/Trash";
 
 const NoteCard = ({ note }) => {
-    const position = JSON.parse(note.position);
+    const [position, setPosition] = useState(JSON.parse(note.position));
     const colors = JSON.parse(note.colors);
     const body = JSON.parse(note.body);
 
     const textAreaRef = useRef(null);
+
+
+    let mouseStartPos = { x: 0, y: 0 };
+
+    const cardRef = useRef(null);
+
+    const mouseMove = (e) => {
+        let mouseMoveDir = {
+            x: mouseStartPos.x - e.clientX,
+            y: mouseStartPos.y - e.clientY
+        };
+
+        //update start position for next move
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+
+        //update card top and left position
+        setPosition({
+            x: cardRef.current.offsetLeft - mouseMoveDir.x,
+            y: cardRef.current.offsetTop - mouseMove.y,
+        })
+    }
+
+    const mouseDown = (e) => {
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+
+        document.addEventListener("mousemove", mouseMove);
+        document.addEventListener("mouseup", mouseUp);
+    }
+
+    const mouseUp = () => {
+        document.removeEventListener("mousemove", mouseMove);
+        document.removeEventListener("mouseup", mouseUp);
+    }
 
     useEffect(() => {
         autoGrow(textAreaRef);
@@ -27,10 +63,12 @@ const NoteCard = ({ note }) => {
                 left: `${position.x}px`,
                 top: `${position.y}px`,
             }}
+            ref={cardRef}
         >
             <div
                 className="card-header"
                 style={{ backgroundColor: colors.colorHeader }}
+                onMouseDown={mouseDown}
             >
                 <Trash />
             </div>
